7e47837750d2f15d361c68e26a8d786f45870baa
WikKam <kamykikamienie@gmail.com>
2020-07-04 19:23:50 +0200

    final i guess

--- src/components/NavbarComponent/NavbarComponent.jsx
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import {Link} from 'react-router-dom';
 
 const NavbarComponent = () => {
     return <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
@@ -11,13 +11,13 @@ const NavbarComponent = () => {
     <Navbar.Collapse id="responsive-navbar-nav">
       <Nav>
         <NavDropdown title="Rankings" id="collasible-nav-dropdown">
-          <NavDropdown.Item href="/topList/coalitions">Coalitions</NavDropdown.Item>
-          <NavDropdown.Item href="/topList/parties">Parties</NavDropdown.Item>
-          <NavDropdown.Item href="/topList/members">Politicians</NavDropdown.Item>
+          <NavDropdown.Item as ={Link} to="/topList/coalitions">Coalitions</NavDropdown.Item>
+          <NavDropdown.Item as ={Link} to="/topList/parties">Parties</NavDropdown.Item>
+          <NavDropdown.Item as ={Link} to="/topList/members">Politicians</NavDropdown.Item>
         </NavDropdown>
         <Nav.Link href="/coalitions">Coalitions</Nav.Link>
-        <Nav.Link eventKey={2} href="/parties">Parties</Nav.Link>
-        <Nav.Link eventKey={2} href="/members">Politicians</Nav.Link>
+        <Nav.Link eventKey={2} as ={Link} to="/parties">Parties</Nav.Link>
+        <Nav.Link eventKey={2} as ={Link} to="/members">Politicians</Nav.Link>
         <Nav.Link href = "https://demagog.org.pl/" target = "_blank">Visit Source</Nav.Link>        
       </Nav>
     </Navbar.Collapse>
