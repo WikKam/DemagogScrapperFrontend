6968f1c093c2ee0a640c51f16f461c17ba1c7da2
WikKam <kamykikamienie@gmail.com>
2020-07-04 19:05:42 +0200

    debugging

--- src/App.js
+++ src/App.js
@@ -6,31 +6,31 @@ import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL || ''}>
     <div className="App">
       <Component/>
       <Switch>
-        <Route exact path="/DemagogScrapperFrontend">
-          <Redirect to = {'/DemagogScrapperFrontend/coalitions'}/>
+        <Route exact path="/">
+          <Redirect to = {'/coalitions'}/>
         </Route>
-        <Route path = {'/DemagogScrapperFrontend/coalitions'} exact render = {(props) => (
+        <Route path = {'/coalitions'} exact render = {(props) => (
           <List type = {'coalitions'} {...props} side = {200} path = {'/coalitions'}></List>
         )}/>
-        <Route path = {'/DemagogScrapperFrontend/coalitions/:name'} render = {(props) => 
+        <Route path = {'/coalitions/:name'} render = {(props) => 
           (<Details {...props} type = 'coalitions'></Details>)}/>
-        <Route path = '/DemagogScrapperFrontend/parties/:name' render = {(props) => 
+        <Route path = '/parties/:name' render = {(props) => 
         (<Details {...props} type = 'parties'></Details>)}/>
-        <Route path = '/DemagogScrapperFrontend/members/:name' render = {(props) => 
+        <Route path = '/members/:name' render = {(props) => 
         (<Details {...props} type = 'members'></Details>)}/>
-        <Route path = '/DemagogScrapperFrontend/parties' exact render = {(props) => 
+        <Route path = '/parties' exact render = {(props) => 
         (<List type = {'parties'} {...props} side = {100} path = {'/parties'}></List>)}/>
-        <Route path = '/DemagogScrapperFrontend/members' exact render = {(props) => 
+        <Route path = '/members' exact render = {(props) => 
         (<List type = {'members'} {...props} side = {100} path = {'/members'}></List>)}/>
-        <Route path = '/DemagogScrapperFrontend/topList/coalitions' render = {(props) => (
+        <Route path = '/topList/coalitions' render = {(props) => (
         <TopList {...props} type = 'coalitions' path = {'/coalitions/results'}></TopList>)}/>
-        <Route path = '/DemagogScrapperFrontend/topList/parties' render = {(props) => (
+        <Route path = '/topList/parties' render = {(props) => (
         <TopList {...props} type = 'parties' path = {'/parties/results'}></TopList>)}/>
-        <Route path = '/DemagogScrapperFrontend/topList/members' render = {(props) => (
+        <Route path = '/topList/members' render = {(props) => (
         <TopList {...props} type = 'members' path = {'/members'}></TopList>)}/>
       </Switch>
     </div>
